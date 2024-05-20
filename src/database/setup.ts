3aeccfbe21113b6900c162af106555e0539e6bbb
Payziii <velmikhail123@gmail.com>
2024-05-20 20:27:39 +0500

    добавил функции создания и получения пользователя

--- src/database/setup.ts
+++ src/database/setup.ts
@@ -1,11 +1,32 @@
 import mongoose from 'mongoose';
 import User from './user.js';
 
-export default async function setupDatabase(bot: any) {
+// Подключение к базе данных и запись схемы в bot
+export function setupDatabase(bot: any) {
 
-mongoose.connect(process.env.MONGODB_URI);
+    mongoose.connect(process.env.MONGODB_URI);
 
-bot.schemas = {};
-bot.schemas.user = User;
+    bot.schemas = {};
+    bot.schemas.user = User;
+}
 
+// Создание нового юзера
+export async function createUser(userID: number) {
+
+    let user = await User.findOne({ userID });
+    if (!user) {
+        user = new User({ userID });
+        await user.save();
+    }
+
+    return user;
+}
+
+// Получение юзера
+export async function getUser(userID: number) {
+
+    let user = await User.findOne({ userID });
+    if (!user) user = await createUser(userID);
+
+    return user;
 }
