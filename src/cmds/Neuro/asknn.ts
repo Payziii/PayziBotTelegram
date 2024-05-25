9d988d961b82cf46c23952731fcfac8d7905f12f
NikitaTumba337 <125444698+KORARAR@users.noreply.github.com>
2024-05-25 12:26:15 +0500

    добавил упрощенное обращение к ии

---
+++ src/cmds/Neuro/asknn.ts
@@ -0,0 +1,32 @@
+// Импортирование необходимых зависимостей и файлов
+import { Context } from "grammy";
+import {
+    type Conversation,
+    type ConversationFlavor,
+} from "@grammyjs/conversations";
+import ask from "../../func/rsnchat.js";
+
+// Создание типов данных
+type MyContext = Context & ConversationFlavor;
+type MyConversation = Conversation<MyContext>;
+
+// Создание команды
+export async function execute(conversation: MyConversation, ctx: MyContext) {
+    await ctx.reply("Введите модель: gpt, gpt4, gemini");
+    const model: any = await conversation.wait();
+    //if(['gpt', 'gpt4', 'gemini'].includes(model.message.text.toLowerCase()) === false) return ctx.reply("Модель указана неверно");
+    //await ctx.reply("Введите ваш запрос");
+    const answer: any = await conversation.wait();
+    ask('gpt4', answer.message.text).then((text) => {
+        ctx.reply(text);
+    })
+}
+
+let data = {
+    command: "ask",
+    description: "задать вопрос chat gpt4",
+    hide: false,
+    active: true
+}
+
+export { data }
