98eda5f387a15cc56f32c5abdade8472b8bf0b88
Payziii <velmikhail123@gmail.com>
2024-10-13 18:56:44 +0500

    фикс

--- src/cmds/conversations.ts
+++ src/cmds/conversations.ts
@@ -6,13 +6,11 @@ import {
 
 import * as weather from './Main/weather.js';
 import * as ask from './Neuro/ask.js';
-import * as gpt4 from './Neuro/gpt4.js';
 
 export default async function setupConversations(bot: any) {
 
     bot.use(conversations());
     bot.use(createConversation(weather.execute, "weather"));
     bot.use(createConversation(ask.execute, "ask"));
-    bot.use(createConversation(gpt4.execute, "gpt4"));
 
 }
